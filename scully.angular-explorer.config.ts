8984b92413d2f1107142a8e1066879d5cfac2cd3
TobiasM <tobias.meyer@maibornwolff.de>
2020-11-22 20:05:22 +0100

    Added Scully

---
+++ scully.angular-explorer.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: './src',
+  projectName: 'angular-explorer',
+  outDir: './dist/static',
+  routes: {
+  }
+};
